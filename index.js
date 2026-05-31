3f61717cfdbc916851473c0ae5a27feebd1e9cf5
Shubham <Shubham>
2026-05-31 12:27:27 +0530

    add nodemailer for password reset

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 
 const { Resend } = require('resend');
-const { Resend } = require('resend');
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
